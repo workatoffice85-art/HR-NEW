4cb6f37811d73c10bfe1f63942272f506485fea5
workatoffice85-art <workatoffice85@gmail.com>
2026-05-12 15:50:32 +0300

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.use(express.json({ limit: '50mb' }));
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static(__dirname));
@@ -17,7 +17,11 @@ app.all('/api/exec', async (req, res) => {
   return handler(req, res);
 });
 
-app.get('*', (req, res) => {
+app.get('*', (req, res, next) => {
+  if (req.path.startsWith('/api')) {
+    return next();
+  }
+
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
